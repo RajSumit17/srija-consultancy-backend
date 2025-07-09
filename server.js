3c51d80cc098928a2cef26fd29cbaaae60cc174f
RajSumit17 <saketraj1793@gmail.com>
2025-07-09 14:44:22 +0530

    Update server.js on CORS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 // App.use(cors())
 App.use(cors({
-  origin: "https://srija-consultancy.netlify.app",
+  origin: ["https://srija-consultancy.netlify.app","http://127.0.0.1:5500"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
